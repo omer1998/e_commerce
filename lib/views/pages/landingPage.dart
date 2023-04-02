c090838d0ed41f3bd803d9e255c15c44c64d89c0
omer <omerfaris11@gmail.com>
2023-04-03 02:35:59 +0300

    add landing page to save the state of logged in user

--- lib/views/pages/landingPage.dart
+++ lib/views/pages/landingPage.dart
@@ -1,4 +1,8 @@
+import 'package:e_commerce/services/auth_service.dart';
+import 'package:e_commerce/views/pages/authPage.dart';
+import 'package:e_commerce/views/pages/bottomNavBar_page.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 
 class LandingPage extends StatelessWidget {
@@ -6,10 +10,27 @@ class LandingPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: Container(child: const Text("landing page")),
-      ),
+    MyFirebaseAuth auth = Provider.of<MyFirebaseAuth>(context, listen: false);
+    return StreamBuilder(
+      stream: auth.authStateChanges(),
+      builder: ((context, snapshot){
+        if(snapshot.connectionState == ConnectionState.active){
+          if(snapshot.hasData){
+            print(snapshot.data);
+            return const BottomNavBar();
+          }else{
+            return const AuthPage();
+        }
+        }else{
+          return const Scaffold(
+            body: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        }
+      }
+
+      )
     );
   }
 }
